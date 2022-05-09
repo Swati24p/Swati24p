da55c9f905dd66cdddc8fbd1ce5df79e033dbf0b
unknown <swatipathak699@gmail.com>
2022-05-09 17:12:11 +0530

    usermodel done

---
+++ src/controller/userController.js
@@ -0,0 +1,19 @@
+const userModel = require ("../models/userModel");
+
+const createUser = async function (req,res){
+ let data = req.body
+    const {title, name, phone, email, password} = data
+
+}
+
+
+
+
+
+
+
+
+
+
+
+module.exports.createUser = createUser;
