ae1facac923689acf871455af20dcb2e29d866b7
Kirtan-G <kirtan942003@gmail.com>
2022-05-24 20:50:35 +0530

    Kirtan Gajjar

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -7,13 +7,13 @@ const authentication = async function (req, res, next) {
         let token = req.headers["x-api-key"];
         if (!token) token = req.headers["X-Api-Key"];
         if (!token) {
-            return res.status(400).send({ Error: "Enter x-api-key In Header" });
+            return res.status(400).send({ Error: "Enter x-api-key In Header !!!" });
         }
 
 
         let checktoken = jwt.verify(token, "GroupNo14");
         if (!checktoken) {
-            return res.status(404).send({ Status: false, msg: "Enter Valid Token" });
+            return res.status(404).send({ Status: false, msg: "Enter Valid Token !!!" });
         }
         next();
     }
